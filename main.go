99b4e40f68ed67eba08eef23f04ecf51a962053a
martinrocket <martin.ruble@gmx.com>
2019-03-28 16:20:31 -0700

    add several HandleFuncs

--- main.go
+++ main.go
@@ -12,16 +12,32 @@ import (
 func main() {
 	fmt.Println("rktStart web server starting @ http://localhost:8080/...")
 
+	r := http.NewServeMux()
+	r.HandleFunc("/api/", handleAPI)
+	r.HandleFunc("/about", handleAbout)
+	r.HandleFunc("/careers", handleCareers)
+	r.HandleFunc("/intro", webServer.RktWebServer)
+	r.HandleFunc("/", webServer.RktStart)
 
-	router := http.NewServeMux()
-	router.HandleFunc("/intro", webServer.RktWebServer)
-	router.HandleFunc("/", webServer.RktStart)
-
-
-
-	err := http.ListenAndServe(":8080", router) // set listen port
+	err := http.ListenAndServe(":8080", r) // set listen port
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
 
+}
+
+func myRoutes() {
+
+}
+
+func handleAPI(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "you got the api")
+}
+
+func handleAbout(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "you got the about")
+}
+
+func handleCareers(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "you got the Careers")
 }

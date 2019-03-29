1191e68ccdd72b37e369afe2b9932a1e8d6e6663
martinrocket <martin.ruble@gmx.com>
2019-03-28 18:24:04 -0700

    Put html into handlefunc.

--- rktHandles.go
+++ rktHandles.go
@@ -6,6 +6,8 @@ import (
   "time"
 )
 
+
+
 func handleAPI(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "you got the api")
 }
@@ -19,12 +21,25 @@ func handleCareers(w http.ResponseWriter, r *http.Request) {
 }
 
 func handleRoot(w http.ResponseWriter, r *http.Request) {
+
 	t := time.Now()
 	fmt.Fprintf(w, "%v\n", t.Format("2006-January-02"))
 	fmt.Fprintf(w, "Started Web Server....")
+
+
 }
 
-
 func handleIntro(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Intro....")
+  fmt.Fprintf(w, `
+    <head>
+      <h2>My Header</h2>
+
+    </head>
+    <body>
+
+        <button>Hello there</button>
+
+    </body>
+
+    `)
 }

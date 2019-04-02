4338a69f909ead5c335a0042203569d3c69897a1
martinrocket <martin.ruble@gmx.com>
2019-04-02 05:39:02 -0500

    add api variables

--- main.go
+++ main.go
@@ -2,16 +2,19 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
 	"runtime"
 )
 
-var version = appInfo{"rktPOS", 0.1, 2019, ""}
+var version = appInfo{"rktPOS", 0.1, 2019, runtime.GOOS}
 
 func main() {
+	clear()
+	posMenu()
 	fmt.Println("Main function running...")
+	fmt.Printf("version call = %v %v %v %v\n", version.name, version.version, version.year, version.os)
 	routes()
-	version.os = runtime.GOOS
-	fmt.Printf("version call = %v\n", version)
 
 }
 
@@ -20,4 +23,10 @@ type appInfo struct {
 	version float64
 	year    int
 	os      string
+}
+
+func clear() {
+	c := exec.Command("clear")
+	c.Stdout = os.Stdout
+	c.Run()
 }
---
+++ rktPOSMenu.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type menu struct {
+	menuName string `json: "menu name"`
+	levelOne
+}
+
+type levelOne struct {
+	List  []string `json: "list"`
+	Close string   `json: "close"`
+}
+
+func posMenu() {
+
+	m := menu{
+		menuName: "Menu Number One",
+		levelOne{
+			List:  []string("List 1"),
+			Close: "x"},
+	}
+
+	n, err := json.MarshalIndent(m, "  ", "  ")
+
+	os.Stdout.Write(n)
+	fmt.Println()
+
+}

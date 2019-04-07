80c998a1e0499c73e424a0061bd7f390b47e67a2
martinrocket <martin.ruble@gmx.com>
2019-04-06 22:48:49 -0600

    start struct for main menu

--- rktPOSMenu.go
+++ rktPOSMenu.go
@@ -3,29 +3,56 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"os"
 )
 
-type menu struct {
-	menuName string `json: "menu name"`
-	levelOne
+//Menu is a struct of Menu Groups and is exported.
+type Menu struct {
+	MenuName  string `json:"menu_name"`
+	Active    bool
+	MenuGroup []MenuItems `json:"menu_group"`
 }
 
-type levelOne struct {
-	List  []string `json: "list"`
-	Close string   `json: "close"`
+type MenuItems struct {
+	ID       int
+	MenuItem string
 }
 
 func posMenu() {
 
-	m := menu{
-		menuName: "Menu Number One"
-		menuName.levelOne.List: {}
+	m1 := &Menu{
+		MenuName: "Menu April 2019",
+		Active:   true,
+		MenuGroup: []MenuItems{
+			MenuItems{
+				ID:       1,
+				MenuItem: "Start of Day",
+			}, {
+				ID:       2,
+				MenuItem: "End of Day",
+			},
+		},
 	}
 
-	n, err := json.MarshalIndent(m, "  ", "  ")
+	m2 := &Menu{}
+	m2.MenuName = "Menu May 2019"
+	m2.Active = false
+
+	n, err := json.MarshalIndent(m1, "", "  ")
+	o, err := json.MarshalIndent(m2, "", "  ")
+
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	os.Stdout.Write(n)
+	fmt.Println()
+	os.Stdout.Write(o)
+	for i := range m1.MenuGroup {
+		fmt.Printf("\n%v:", m1.MenuGroup[i].MenuItem)
+	}
+
 	fmt.Println()
 
 }
